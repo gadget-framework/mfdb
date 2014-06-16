4da180afb51ccbf62590b18dd63036f6269f6fe3
Jamie Lentin <jm@lentin.co.uk>
2014-06-16 13:56:25 +0100

    Test writing files

--- tests/testthat/test-gadget_directory.R
+++ tests/testthat/test-gadget_directory.R
@@ -10,3 +10,21 @@ test_that("Will create a directory when creating gadget_directories", {
     gd <- gadget_directory(dir)
     expect_true(file.exists(dir) && file_test("-d", dir))
 })
+
+test_that("Can write files", {
+    # Create a temporary directory, starts off empty
+    dir <- tempfile()
+    gd <- gadget_directory(dir)
+    expect_equal(list.files(dir), character(0))
+
+    # Write out files, come back the same
+    gf_animals <- gadget_file("animals", components = list(list(cow = "Daisy", pig = "George")))
+    gadget_dir_write(gd, gf_animals)
+    expect_equal(gadget_dir_read(gd, "animals"), gf_animals)
+    expect_equal(list.files(dir), c("animals"))
+
+    gf_plants <- gadget_file("plants", components = list(list(tree = "The larch", tree = "The pine")))
+    gadget_dir_write(gd, gf_plants)
+    expect_equal(gadget_dir_read(gd, "plants"), gf_plants)
+    expect_equal(list.files(dir), c("animals", "plants"))
+})
