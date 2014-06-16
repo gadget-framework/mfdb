1a9e803e049136c6ba3eafcf8fa4ea39bf6cc239
Jamie Lentin <jm@lentin.co.uk>
2014-06-16 15:54:21 +0100

    Add tests for likelihood components

--- tests/testthat/test-gadget_directory.R
+++ tests/testthat/test-gadget_directory.R
@@ -28,3 +28,39 @@ test_that("Can write files", {
     expect_equal(gadget_dir_read(gd, "plants"), gf_plants)
     expect_equal(list.files(dir), c("animals", "plants"))
 })
+
+test_that("Can write likelihood components", {
+    # Create a temporary directory, starts off empty
+    dir <- tempfile()
+    gd <- gadget_directory(dir)
+    expect_equal(list.files(dir), character(0))
+
+    # Create some components
+    gadget_dir_write(gd, gadget_likelihood_component("understocking", name="head-bone", weight = 0.3))
+    expect_equal(list.files(dir), c("likelihood"))
+    expect_equal(
+        gadget_dir_read(gd, "likelihood")$components,
+        list(
+            list(),
+            component = list(name = "head-bone", weight = 0.3, type = "understocking")))
+
+    gadget_dir_write(gd, gadget_likelihood_component("penalty", name="neck-bone", weight = 0.5))
+    expect_equal(list.files(dir), c("likelihood", "neck-bone.penaltyfile"))
+    expect_equal(
+        gadget_dir_read(gd, "likelihood")$components,
+        list(
+            list(),
+            component = list(name = "head-bone", weight = 0.3, type = "understocking"),
+            component = list(name = "neck-bone", weight = 0.5, type = "penalty", datafile = "neck-bone.penaltyfile")))
+
+    # Override one, add another
+    gadget_dir_write(gd, gadget_likelihood_component("understocking", name="head-bone", weight = 0.8))
+    gadget_dir_write(gd, gadget_likelihood_component("understocking", name="shoulder-bone", weight = 0.2))
+    expect_equal(
+        gadget_dir_read(gd, "likelihood")$components,
+        list(
+            list(),
+            component = list(name = "head-bone", weight = 0.8, type = "understocking"),
+            component = list(name = "neck-bone", weight = 0.5, type = "penalty", datafile = "neck-bone.penaltyfile"),
+            component = list(name = "shoulder-bone", weight = 0.2, type = "understocking")))
+})
