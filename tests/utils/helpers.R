a5a17f8e007dbd3bb138f900fbfd5f2ed676e5c9
Jamie Lentin <jm@lentin.co.uk>
2014-10-15 10:57:41 +0100

    Remove comparision functions from ok wrappers

--- tests/utils/helpers.R
+++ tests/utils/helpers.R
@@ -8,17 +8,23 @@ print.stringvec <- function(x, ...) {
     cat(x, sep = "\n")
 }
 
-expect_equal <- function(actual, expected) {
-    compare <- function(a, b) {
-        if(identical(all.equal(a,b), TRUE)) return(TRUE)
+cmp <- function(a, b) {
+    if(identical(all.equal(a,b), TRUE)) return(TRUE)
 
-        return(c(
-            capture.output(str(actual)),
-            "... does not equal...",
-            capture.output(str(expected))
-        ))
-    }
-    ok(compare(actual, expected), paste0(
+    return(c(
+        capture.output(str(a)),
+        "... does not equal...",
+        capture.output(str(b))
+    ))
+}
+
+cmp_error <- function(exp, expected_regexp) {
+    msg <- tryCatch({exp ; "No error returned"}, error = function(e) e$message)
+    if(grepl(expected_regexp, msg)) TRUE else paste0("'", msg, "' should contain '", expected_regexp, "'")
+}
+
+expect_equal <- function(actual, expected) {
+    ok(cmp(actual, expected), paste0(
         strtrim(gsub("\\s+", " ", deparse(substitute(actual)), perl = TRUE), 30),
         " == ",
         strtrim(gsub("\\s+", " ", deparse(substitute(expected)), perl = TRUE), 30),
@@ -26,8 +32,7 @@ expect_equal <- function(actual, expected) {
 }
 
 expect_error <- function(exp, expected_regexp) {
-    msg <- tryCatch({exp ; "No error returned"}, error = function(e) e$message)
-    ok(grepl(expected_regexp, msg), paste0("'", msg, "' should contain '", expected_regexp, "'"))
+    ok(cmp_error(exp, expected_regexp), paste0("Error contained '", expected_regexp, "'"))
 }
 
 expect_true <- function(exp) {
