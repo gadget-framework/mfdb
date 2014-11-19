0ed03cd8057af8e33263d8ea174588a83662b3bc
Jamie Lentin <jm@lentin.co.uk>
2014-11-19 12:08:39 +0000

    Update example with stuff that should work, although this file's days are numbered

--- demo/example.R
+++ demo/example.R
@@ -1,44 +1,33 @@
-# options(error=utils::recover)
 library(mfdb)
 library(logging)
 
+# Stop on errors
 options(error=utils::recover)
+
+# See as much detail as we can about what mfdb is up to
 addHandler(writeToConsole, logger='mfdb', level='DEBUG')
 
-# Define options we need to add to get catch data
-opt_catch <- list()
-#    sampling_type = c(130, 131, 133))
+# Store the default params we use when querying
+defaults <- list(
+    area = mfdb_group(
+        "101" = c(1011, 1012, 1013, 1014, 1015),
+        "102" = c(1021, 1022, 1023)),
+    timestep = mfdb_group_numbered("ts", c(1,2,3,4,5,6), c(7,8,9,10,11,12)), # Group months to create 2 timesteps for each year
+    null = NULL)
 
-# Connect to the given dst2dw database, and set some default
-# parameters to use when querying
-mdb <- mfdb(list(dbname="dw0605", host="/tmp/"),
-    defaultparams = list(
-        area = mfdb_group(
-            "101" = c(1011, 1012, 1013, 1014, 1015),
-            "102" = c(1021, 1022, 1023)),
-        timestep = mfdb_group_numbered("ts", c(1,2,3,4,5,6), c(7,8,9,10,11,12)), # Group months to create 2 timesteps for each year
-        null = NULL),
-    save_temp_tables = FALSE)
-# NB: Any of these parameters can be overriden when performing a query
+# Connect to a local database, don't keep temporary tables
+mdb <- mfdb(save_temp_tables = FALSE)
 
-# Initalise a gadget directory for output.
-gd <- gadget_directory("./out")
-# NB: Could have a "remove everything" option here, but it doesn't yet exist.
+# Initalise a gadget directory for output, will create it if it doesn't exist
+gd <- gadget_directory("out")
 
 # Fetch sizes and temperatures for area, and turn them into an areafile.
-# sizes <- mfdb_area_size(mdb)
-# temps <- mfdb_temperatures(mdb)
-# gadget_dir_write(gadget_areafile(gd,
-#         sizes = sizes,
-#        temperatures = temps))
-# NB: I haven't implemented the above, but it's an example of how it might work
+gadget_dir_write(gadget_areafile(gd,
+    size = mfdb_area_size(mdb, defaults),
+    temperature = mfdb_temperature(mdb, defaults)))
 
 # Add a likelihood component for penalty, based on a data.frame
-# NB: Currently this just appends components to the likelihood file, however
-# an improvement would be to parse the likelihood file, find any existing components
-# with the same name/type combination, and replace them if they exist.
-# This allows you to regenerate the sections that need regenerating, and leave the
-# others untouched.
+# If there is already a penalty component called "bounds", then this will replace it
 gadget_dir_write(gd, gadget_likelihood_component("penalty",
         name = "bounds",
         weight = "0.5",
@@ -48,7 +37,7 @@ gadget_dir_write(gd, gadget_likelihood_component("penalty",
             stringsAsFactors = FALSE)))
 
 # Get the mean length data from the database, override the areas set in
-# defaultparams above and add a few more.
+# defaults above and add a few more.
 agg_data <- mfdb_meanlength_stddev(mdb,
         params = c(list(
             year = c(1990, 1991, 1992, 1993),
@@ -57,33 +46,38 @@ agg_data <- mfdb_meanlength_stddev(mdb,
                 "102" = c(1021, 1022, 1023))), # NB: Add one more & bootstrap, overriding default
             age = mfdb_group('young' = c(1,2,3), 'old' = c(4,5,6)),
             length = mfdb_interval("len", seq(0, 500, by = 50)),
-            # species = "COD",
-            null = NULL), opt_catch))
+            species = "COD",
+            null = NULL), defaults))
+# NB: Since we requested 10 bootstrap samples, agg_data is a vector of 10 data.frames,
+# each is annotated with the age/area grouping information to allow us to write
+# a likelihood component out to the gadget directory.
+# NB: Any additional data manipulation could be performed at this stage, as
+# long as the resulting data structure matches.
 
-# NB: Since we requested 10 bootstrap samples, agg_data is a vector of 10 items,
-# each is essentially a data.frame that contains the final data. The data here
-# has been wrapped up into divisions, ready to be outputted to a gadget file.
-# This is for speed, although there's no reason not to specify each individual
-# subdivision and then do the aggregation within R.
-
-# Write this data out into a datafile, as well as age and area files. Add the
-# component entry to the bottom of the likelihood file
-# NB: function is worked out from the input data, although can be overridden.
+# Write this component out. In one step, this:-
+# * Writes datafile, agefile, areafile to GADGET directory
+# * Add / replace codstatistics component in likelihood file
+# * Ensure likelihood file is in main data file
 gadget_dir_write(gd, gadget_likelihood_component("catchstatistics",
         name = "codstatistics",
         weight = 0.8,
         data = agg_data[[1]],
         fleetnames = c("igfs"),
         stocknames = c("codimm", "codmat")))
-rm(agg_data) # Free up memory before moving on to the next component
+# NB: The catchstatistics function is guessed, based on the input data.
 
+# The data.frames are potentially quite large data structures. Free up
+# memory before moving on to the next component
+rm(agg_data)
+
+# Another likelihood compoment
 agg_data <- mfdb_meanweight(mdb,
         params = c(list(
             year = c(1990, 1991, 1992, 1993),
             age = mfdb_group_numbered("age", c(1), c(2), c(3), c(4), c(5)),
             length = mfdb_interval("len", seq(0, 500, by = 50)),
             # species = "COD",
-            null = NULL), opt_catch))
+            null = NULL), defaults))
 
 gadget_dir_write(gd, gadget_likelihood_component("catchstatistics",
         name = "weightstatistics",
@@ -91,17 +85,15 @@ gadget_dir_write(gd, gadget_likelihood_component("catchstatistics",
         data = agg_data[[1]]))
 rm(agg_data) # Free up memory before moving on to the next component
 
+# Another likelihood compoment
 agg_data <- mfdb_agelength(mdb,
         params = c(list(
             year = c(1990, 1991),
             length = mfdb_interval("len", seq(0, 500, by = 50)),
             age = mfdb_group('young' = c(1,2,3)),
-            null = NULL), opt_catch))
+            null = NULL), defaults))
 gadget_dir_write(gd, gadget_likelihood_component("catchdistribution",
         name = "cdist",
         weight = 0.9,
         data = agg_data[[1]]))
 rm(agg_data) # Free up memory before moving on to the next component
-
-# Create a mainfile with everything that has been created so far
-# gadget_mainfile(gd)
