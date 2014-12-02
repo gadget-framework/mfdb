a36d6265736a85f6b684dc32076ab60bc89f7072
Jamie Lentin <jm@lentin.co.uk>
2014-12-02 14:17:34 +0000

    Tidy up example scripts a bit

---
+++ demo/example-datras.R
@@ -0,0 +1,121 @@
+# To run this script, first install data sources:-
+#  Debian / Ubuntu: apt-get install php5-cli php5-curl
+# install.packages('DATRAS', repos = 'http://www.rforge.net/', type = 'source')
+library(DATRAS)
+library(mfdb)
+library(plyr)
+
+# Download data and read in available data for cod
+if (!file.exists('example-datras-data/NS-IBTS_1995.zip')) {
+    dir.create('example-datras-data', recursive = TRUE, showWarnings = FALSE)
+    oldwd <- getwd()
+    setwd('example-datras-data')
+    downloadExchange("NS-IBTS",1995:2005)
+    setwd(oldwd)
+}
+d <- readExchangeDir(path = 'example-datras-data')
+cod <- subset(d,Species == 'Gadus morhua')
+
+# Retrieve length data
+codL <- merge(cod[['HL']],cod[['HH']][c('haul.id','lat','lon')])
+codL$areacell <- icesSquare(codL)
+codL$Species <- 'COD'
+
+# Retrieve age data
+codA <- merge(cod[['CA']],cod[['HH']][c('haul.id','lat','lon')])
+codA$areacell <- as.character(codA$AreaCode)
+codA$Species <- 'COD'
+
+# Create connection to MFDB database, as the Baltic case study
+mdb <- mfdb('Baltic')
+
+# Assume all squares are 5km^2
+squares <- unique(c(codA$areacell,codL$areacell))
+mfdb_import_area(mdb, data.frame(
+    id = 1:length(squares),
+    name = squares,
+    size = c(5)))
+
+# Create divisions and import those
+sub.split <- llply(squares, function(x) x)                           
+names(sub.split) <- squares
+mfdb_import_division(mdb, sub.split)
+
+# Hard-code temperature data (TODO: Find proper source)
+mfdb_import_temperature(mdb, data.frame(
+    year = 1996:2005,
+    month = 1,
+    areacell = squares[1],
+    temperature = 7.4))
+
+# Set-up some sampling types
+mfdb_import_sampling_type(mdb, data.frame(
+    id = 1,
+    name = 'RES',
+    description = 'Research'))
+
+# Import cod length data as a survey
+tmp <- codL[c('Year','Quarter','areacell','Species','Sex','LngtCm','Count')]
+names(tmp) <- c('year','month','areacell','species','sex','length','count')
+tmp <- subset(tmp,!is.na(length))
+mfdb_import_survey(mdb,
+                   data_source = 'example-datras-codL',
+                   sampling_type = 'RES',
+                   tmp)
+
+# Import cod age data as a separate survey
+tmp <- codA[c('Year','Quarter','areacell','Species','Sex','Age','LngtCm')]
+names(tmp) <- c('year','month','areacell','species','sex','age','length')
+tmp <- subset(tmp,!is.na(age))
+mfdb_import_survey(mdb,
+                   data_source = 'example-datras-codA',
+                   sampling_type = 'RES',
+                   tmp)
+
+# Create a gadget directory, define some defaults to use with our queries below
+gd <- gadget_directory("example-datras-model")
+defaults <- list(
+    area = mfdb_group("101" = squares),
+    timestep = mfdb_timestep_quarterly, # Group months to create 2 timesteps for each year
+    year = 1996:2005)
+
+# Write out areafile and update mainfile with areafile location
+gadget_dir_write(gd,gadget_areafile(
+    size = mfdb_area_size(mdb, defaults)[[1]],
+    temperature = mfdb_temperature(mdb, defaults)[[1]]))
+
+# Write a penalty component to the likelihood file
+gadget_dir_write(gd, gadget_likelihood_component("penalty",
+                                                 name = "bounds",
+                                                 weight = "0.5",
+                                                 data = data.frame(
+                                                   switch = c("default"),
+                                                   power = c(2),
+                                                   stringsAsFactors = FALSE)))
+
+# Query some age-length data and write it as a catchdistribution component
+aggdata <- mfdb_agelength(mdb, c(list(
+    age = mfdb_group_numbered('age',1,2,3,4,5,6,7,8,9,10,11,12,13),
+    sampling_type = 'RES',
+    length = mfdb_interval("len", seq(0, 150, by = 2))),
+    defaults))
+gadget_dir_write(gd, gadget_likelihood_component("catchdistribution",
+                                                 name = "aldist.datras",
+                                                 weight = 1,
+                                                 data = aggdata[[1]],
+                                                 fleetnames = c("datras"),
+                                                 stocknames = 'cod'))
+rm(aggdata)
+
+# Query some length data and write a separate component (note different name)
+Length.query <- mfdb_agelength(mdb, c(list(
+    sampling_type = 'RES',
+    length = mfdb_interval("len", seq(0, 150, by = 2))),
+    defaults))
+gadget_dir_write(gd, gadget_likelihood_component("catchdistribution",
+                                                 name = "ldist.datras",
+                                                 weight = 1,
+                                                 data = Length.query[[1]],
+                                                 fleetnames = c("datras"),
+                                                 stocknames = 'cod'))
+rm(aggdata)
--- demo/example-iceland.R
+++ demo/example-iceland.R
@@ -1,49 +1,40 @@
+# To run this script, first install data sources: fjolst, geo
 library(mfdb)
 library(fjolst)
 library(geo)
 library(plyr)
 library(data.table)
-reitmapping <-
-  read.table('reitmapping.tsv',header=TRUE,as.is=TRUE)
 
-sub.split <- dlply(reitmapping, 'SUBDIVISION', function(x) x$GRIDCELL)
-defaults <- list(
-  area = mfdb_group(
-    "101" = reitmapping$SUBDIVISION),
-  timestep = mfdb_timestep_quarterly, # Group months to create 2 timesteps for each year
-  year = 1984:2012)
-
-
+# Create connection to MFDB database, as the Icelandic case study
 mdb <- mfdb('Iceland')
 
-
-gd <- gadget_directory("out")
-
-
-## area setup
-mfdb_import_area(mdb, data.frame(id = 1:nrow(reitmapping), name = reitmapping$GRIDCELL, size = c(5)))
-mfdb_import_division(mdb, sub.split)
+# Import area definitions
+reitmapping <- read.table(
+    system.file("demo-data", "reitmapping.tsv", package="mfdb"),
+    header=TRUE,
+    as.is=TRUE)
+mfdb_import_area(mdb, data.frame(
+    id = 1:nrow(reitmapping),
+    name = reitmapping$GRIDCELL,
+    size = c(5)))
+mfdb_import_division(mdb, dlply(reitmapping, 'SUBDIVISION', function(x) x$GRIDCELL))
 mfdb_import_temperature(mdb, data.frame(
-  year = 2012,
-  month = 1:12,
-  areacell = reitmapping$GRIDCELL[1],
-  temperature = 3))
+    year = 2012,
+    month = 1:12,
+    areacell = reitmapping$GRIDCELL[1],
+    temperature = 3))
 
-gadget_dir_write(gd,gadget_areafile(
-                                 size = mfdb_area_size(mdb, defaults)[[1]],
-                                 temperature = mfdb_temperature(mdb, defaults)[[1]]))
+# Set-up some sampling types
+mfdb_import_sampling_type(mdb, data.frame(
+    id = 1:2,
+    name = c('SEA', 'IGFS'),
+    description = c('Sea sampling', 'Icelandic ground fish survey')))
 
-## Actual data 
-## 1 inspectors, 2 hafro, 8 on-board discard 
-stations <- subset(stodvar, synaflokkur == 30) ## autumn survey 35
+# Import length distribution from commercial catches
+# 1 inspectors, 2 hafro, 8 on-board discard
 comm.stations <- subset(stodvar, synaflokkur == 8)
-ldist <- subset(all.le, synis.id %in% stations$synis.id & tegund == 1)
 comm.ldist <- subset(all.le, synis.id %in% comm.stations$synis.id & 
                        tegund == 1)
-# adist <- subset(all.kv, synis.id %in% stations$synis.id & tegund == 1 )
-num.dist <- subset(all.nu, synis.id %in% stations$synis.id & tegund == 1 )
-ldist.aug <- Skala.med.toldum(ldist,num.dist, tegund==1)
-# adist.aug <- Skala.med.toldum(adist,num.dist, tegund==1)
 
 comm.ldist <- merge(comm.ldist, 
                     comm.stations[c('synis.id','ar','man','lat','lon')])
@@ -54,13 +45,17 @@ comm.ldist$species <- 'COD'
 comm.ldist <- subset(comm.ldist, areacell %in% reitmapping$GRIDCELL) 
 comm.ldist$sex <- c('M','F')[comm.ldist$sex]
 comm.ldist$age <- 0
-
 mfdb_import_survey(mdb,
-                   data_source = 'comm-test',
+                   data_source = 'example-iceland-comm.ldist',
                    sampling_type = 'SEA',
                    comm.ldist)
+rm(comm.ldist)
 
-
+# Import length distribution from autumn survey
+stations <- subset(stodvar, synaflokkur == 30) ## autumn survey 35
+ldist <- subset(all.le, synis.id %in% stations$synis.id & tegund == 1)
+num.dist <- subset(all.nu, synis.id %in% stations$synis.id & tegund == 1)
+ldist.aug <- Skala.med.toldum(ldist,num.dist, tegund==1)
 ldist.aug <- merge(ldist.aug, stations[c('synis.id','ar','lat','lon')])
 ldist.aug$areacell <- d2sr(ldist.aug$lat,-ldist.aug$lon)
 names(ldist.aug) <- c('sample.id','species','length','num','sex',
@@ -72,18 +67,25 @@ ldist.aug <- subset(ldist.aug,!is.na(areacell))
 ldist.aug <- subset(ldist.aug,areacell %in% reitmapping$GRIDCELL) 
 ldist.aug$sex <- c('M','F')[ldist.aug$sex]
 ldist.aug$age <- 0
-  mfdb_import_survey(mdb,
-                     data_source = 'igfs-test',
-                     sampling_type = 'IGFS', 
-                     ldist.aug)
+mfdb_import_survey(mdb,
+                   data_source = 'example-iceland-ldist.aug',
+                   sampling_type = 'IGFS', 
+                   ldist.aug)
+rm(ldist.aug)
 
-## lets do something useful
-comm.query <- 
-mfdb_agelength(mdb, c(list(
-  sampling_type = 'SEA',
-  length = mfdb_interval("len", seq(0, 150, by = 2))),
-  defaults))
+# Create a gadget directory, define some defaults to use with our queries below
+gd <- gadget_directory("out")
+defaults <- list(
+    area = mfdb_group("101" = reitmapping$SUBDIVISION),
+    timestep = mfdb_timestep_quarterly,
+    year = 1984:2012)
 
+# Write out areafile and update mainfile with areafile location
+gadget_dir_write(gd, gadget_areafile(
+    size = mfdb_area_size(mdb, defaults)[[1]],
+    temperature = mfdb_temperature(mdb, defaults)[[1]]))
+
+# Write a penalty component to the likelihood file
 gadget_dir_write(gd, gadget_likelihood_component("penalty",
                                                  name = "bounds",
                                                  weight = "0.5",
@@ -92,19 +94,28 @@ gadget_dir_write(gd, gadget_likelihood_component("penalty",
                                                    power = c(2),
                                                    stringsAsFactors = FALSE)))
 
-
+# Query length data to create some catchdistribution components
+aggdata <- mfdb_agelength(mdb, c(list(
+  sampling_type = 'IGFS',
+  length = mfdb_interval("len", seq(0, 150, by = 2))),
+  defaults))
 gadget_dir_write(gd, gadget_likelihood_component("catchdistribution",
                                                  name = "ldist.igfs",
                                                  weight = 1,
-                                                 data = tmp[[1]],
+                                                 data = aggdata[[1]],
                                                  fleetnames = c("igfs"),
                                                  stocknames = c("codimm", "codmat")))
+rm(aggdata)
 
 
+aggdata <- mfdb_agelength(mdb, c(list(
+  sampling_type = 'SEA',
+  length = mfdb_interval("len", seq(0, 150, by = 2))),
+  defaults))
 gadget_dir_write(gd, gadget_likelihood_component("catchdistribution",
                                                  name = "ldist.comm",
                                                  weight = 1,
-                                                 data = tmp[[1]],
+                                                 data = aggdata[[1]],
                                                  fleetnames = c("comm"),
                                                  stocknames = c("codimm", "codmat")))
-
+rm(aggdata)
