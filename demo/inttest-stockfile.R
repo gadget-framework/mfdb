b19478f2f97f45f03bff6c9e4b095fc9188dbe19
Jamie Lentin <jm@lentin.co.uk>
2015-03-16 18:21:57 +0000

    Test updates of a stockfile

--- demo/inttest-stockfile.R
+++ demo/inttest-stockfile.R
@@ -108,7 +108,7 @@ year	month	areacell	species	maturity_stage	length	age	weight
         ver_string,
         "; -- data --",
         "; length\tweight",
-        "30\t223.333333333333",  #TODO: Rounding?
+        "30\t223.333333333333",
         "40\t330",
         "50\t380",
         "60\t320",
@@ -214,7 +214,7 @@ ok_group("refweight files are always continuous and in length order", {
         ver_string,
         "; -- data --",
         "; length\tweight",
-        "30\t223.333333333333",  #TODO: Rounding?
+        "30\t223.333333333333",
         "40\t330",
         "50\t380",
         "60\t320",
@@ -222,8 +222,92 @@ ok_group("refweight files are always continuous and in length order", {
 })
 
 ok_group("Stockfile updates don't wreck existing files", {
-    # Create a temporary gadget directory
+    # Create a temporary gadget directory, populate mature cod file
     gd <- gadget_directory(tempfile())
+    mat_data <- mfdb_sample_meanweight(mdb, c('age', 'length'), list(
+        age = NULL, # This means the age column will have "all", but aggfile will have min & max
+        length = mfdb_step_interval('len', 10, to = 100),
+        species = 'COD',
+        maturity_stage = '5',
+        null = NULL))
+    gadget_dir_write(gd, gadget_stockfile_extremes('cod.mat', mat_data[[1]]))
+    gadget_dir_write(gd, gadget_stockfile_refweight('cod.mat', mat_data[[1]]))
+    mat_data <- mfdb_sample_meanweight(mdb, c('age', 'length'), list(
+        year = 2000:2003,
+        step = mfdb_timestep_quarterly,
+        area = mfdb_unaggregated(),
+        age = NULL, # This means the age column will have "all", but aggfile will have min & max
+        length = mfdb_step_interval('len', 5, from = 50, to = 100),
+        species = 'COD',
+        maturity_stage = '5',
+        null = NULL))
+    gadget_dir_write(gd, gadget_stockfile_recruitment('cod.mat', mat_data[[1]]))
 
-    #TODO:
+    # File should now have a bunch of content
+    ok(cmp_file(gd, "Modelfiles/cod.mat",
+       ver_string,
+        "stockname\tcod.mat",
+        "minage\t3",
+        "maxage\t3",
+        "minlength\t0",
+        "maxlength\t100",
+        "dl\t10",
+        "refweightfile\tModelfiles/cod.mat.refwgt",
+        "doesrenew\t1",
+        "minlength\t50",
+        "maxlength\t100",
+        "dl\t5",
+        "numberfile\tModelfiles/cod.mat.rec.number",
+        NULL), "Stockfile has initial content")
+
+    # Manually open file and tinker with iseaten
+    gf <- gadget_dir_read(gd, "Modelfiles/cod.mat", file_type = c("bare_component", implicit_component = "^(growthandeatlengths|doesgrow|naturalmortality|iseaten|doeseat|doesmigrate|doesmature|doesmove|doesrenew|doesspawn|doesstray)$"))
+    gf$components$iseaten <- list(iseaten = 1, lots = "yes-is-tasty")
+    gadget_dir_write(gd, gf)
+    ok(cmp_file(gd, "Modelfiles/cod.mat",
+       ver_string,
+        "stockname\tcod.mat",
+        "minage\t3",
+        "maxage\t3",
+        "minlength\t0",
+        "maxlength\t100",
+        "dl\t10",
+        "refweightfile\tModelfiles/cod.mat.refwgt",
+        "doesrenew\t1",
+        "minlength\t50",
+        "maxlength\t100",
+        "dl\t5",
+        "numberfile\tModelfiles/cod.mat.rec.number",
+        "iseaten\t1",
+        "lots\tyes-is-tasty",
+        NULL), "iseaten added to end")
+
+    # Write new recruitment, iseaten not affected
+    mat_data <- mfdb_sample_meanweight(mdb, c('age', 'length'), list(
+        year = 2000:2003,
+        step = mfdb_timestep_quarterly,
+        area = mfdb_unaggregated(),
+        age = NULL, # This means the age column will have "all", but aggfile will have min & max
+        length = mfdb_step_interval('len', 2, from = 50, to = 70),
+        species = 'COD',
+        maturity_stage = '5',
+        null = NULL))
+    gadget_dir_write(gd, gadget_stockfile_recruitment('cod.mat', mat_data[[1]]))
+    ok(cmp_file(gd, "Modelfiles/cod.mat",
+       ver_string,
+        "stockname\tcod.mat",
+        "minage\t3",
+        "maxage\t3",
+        "minlength\t0",
+        "maxlength\t100",
+        "dl\t10",
+        "refweightfile\tModelfiles/cod.mat.refwgt",
+        "doesrenew\t1",
+        "minlength\t50",
+        "maxlength\t70",
+        "dl\t2",
+        "numberfile\tModelfiles/cod.mat.rec.number",
+        "iseaten\t1",
+        "lots\tyes-is-tasty",
+        NULL), "iseaten still at end, only doesrenew changed")
 })
