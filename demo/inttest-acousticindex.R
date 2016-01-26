3a78092de62c68b1eaf589b7cd10682107725b72
Jamie Lentin <jm@lentin.co.uk>
2016-01-26 16:04:44 +0000

    No point testing that we can rename worms multiple times

--- demo/inttest-acousticindex.R
+++ demo/inttest-acousticindex.R
@@ -17,12 +17,7 @@ avg <- function (...) {
 # Empty database
 if (exists("mdb")) mfdb_disconnect(mdb)
 mfdb('', db_params = db_params, destroy_schema = TRUE)
-
-# Rebuild database, taxonomy got populated
 mdb <- mfdb('Test', db_params = db_params, save_temp_tables = FALSE)
-ok(all(mfdb:::mfdb_fetch(mdb, "SELECT name, description FROM species WHERE species_id = 9999999999")[1,] == 
-  mfdb::species[mfdb::species$name == 'TBX', c('name', 'description')]), "Entry for 9999999999 matches package")
-ok(cmp(mfdb:::mfdb_fetch(mdb, "SELECT count(*) FROM species")[1,1], nrow(mfdb::species)), "Species has right number of entries")
 
 # Gadget directory
 gd <- gadget_directory(tempfile())
