56d44fbd6bea309b39320f068951de51d417fd1f
Jamie Lentin <jm@lentin.co.uk>
2020-04-03 17:19:13 +0100

    gadget_fleetfile: Testable examples

--- R/mfdb_example_data.R
+++ R/mfdb_example_data.R
@@ -21,4 +21,25 @@ mfdb_populate_example_data <- function (mdb) {
         divA = c('45G03'),
         divB = c('45G01', '45G02'),
         divC = c('45G01')))
+
+    mfdb_import_vessel_taxonomy(mdb, data.frame(
+        name = c('1.RSH', '2.COM'),
+        stringsAsFactors = FALSE
+    ))
+    mfdb_import_survey(mdb, data_source = "fleet_component_example",
+        table_string("
+year    month   areacell        species vessel  length  age     weight
+1998    1       45G01           COD     1.RSH           21      2       210
+1998    1       45G02           COD     2.COM           34      3       220
+1998    1       45G03           COD     2.COM           34      3       230
+1998    1       45G01           COD     2.COM           62      1       320
+1998    1       45G02           COD     2.COM           53      1       330
+1998    1       45G03           COD     2.COM           54      2       430
+1998    1       45G01           COD     1.RSH           28      2       210
+1998    1       45G02           COD     2.COM           34      3       220
+1998    1       45G03           COD     1.RSH           24      3       230
+1998    1       45G01           COD     1.RSH           12      1       320
+1998    1       45G02           COD     2.COM           44      1       330
+1998    1       45G03           COD     1.RSH           14      2       430
+        "))
 }
