bed1b72ef066df805b182ec971f7f8cf9e044a99
Jamie Lentin <jm@lentin.co.uk>
2020-04-03 17:17:55 +0100

    gadget_areafile: testable examples

--- R/mfdb_example_data.R
+++ R/mfdb_example_data.R
@@ -12,4 +12,13 @@ mfdb_populate_example_data <- function (mdb) {
         id = c(1,2,3),
         name = c('45G01', '45G02', '45G03'),
         size = c(5)))
+    mfdb_import_temperature(mdb, data.frame(
+        year = rep(c(1998, 1999), each = 12),
+        month = c(1:12, 1:12),
+        areacell = c(rep('45G01', times = 24)),
+        temperature = c(1:12, 25:36)))
+    mfdb_import_division(mdb, list(
+        divA = c('45G03'),
+        divB = c('45G01', '45G02'),
+        divC = c('45G01')))
 }
