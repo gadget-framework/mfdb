5517c611b6c8768c1f3c7779f373d52e4f3d630b
Jamie Lentin <jm@lentin.co.uk>
2020-04-09 11:16:51 +0100

    test-mfdb: Test MFDB_FORCE_AVAILABLE works

--- tests/test-mfdb.R
+++ tests/test-mfdb.R
@@ -73,6 +73,11 @@ ok_group("mfdb", {
             list(host = "mfdb.rhi.hi.is", user = "polly", password = "ppwd", dbname = "dbname=mf sslmode=require", drv = 'PostgreSQLDriver')
         )), "Used special mfdb server parameters")
 
+        old_force <- Sys.getenv('MFDB_FORCE_AVAILABLE')
+        Sys.setenv(MFDB_FORCE_AVAILABLE = "")
         ok(cmp(mfdb(check_db_available = T), FALSE), "Didn't error with check_db_available, just return FALSE")
+        Sys.setenv(MFDB_FORCE_AVAILABLE = "T")
+        ok(cmp(mfdb(check_db_available = T), TRUE), "MFDB_FORCE_AVAILABLE on, returned TRUE anyway")
+        Sys.setenv(MFDB_FORCE_AVAILABLE = old_force)
     })
 })
