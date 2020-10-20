e42cb48ce2f87d399cad3d3f881f1f82ef5dba8f
Jamie Lentin <jm@lentin.co.uk>
2020-10-20 09:39:22 +0100

    mfdb_dplyr: Use newer dplyr syntax
    The {{ "operator" is a rlang 0.4.0 addition, which is required by dplyr
    0.8.3 onwards.

--- R/mfdb_dplyr.R
+++ R/mfdb_dplyr.R
@@ -3,14 +3,6 @@ all_cols <- c("$ALL")
 
 # Fetch a dplyr version of any measurement table
 mfdb_dplyr_table <- function (mdb, tbl_name, include_cols) {
-    # Given (name), (value), (name), (value), ..., make a list
-    named_list <- function (...) {
-        name_and_items <- c(...)
-        structure(
-            (name_and_items)[(seq_along(name_and_items) %% 2) == 1],
-            names = (name_and_items)[(seq_along(name_and_items) %% 2) == 0])
-    }
-
     # Turn mdb connection into dplyr & get table
     #dp <- mdb$db#dbplyr::src_sql("postgres", mdb$db, info = DBI::dbGetInfo(mdb$db))
     dp_tbl <- dplyr::tbl(mdb$db, tbl_name)
@@ -28,15 +20,15 @@ mfdb_dplyr_table <- function (mdb, tbl_name, include_cols) {
     # Left-join each desired taxonomy table
     for (tx_col in intersect(tx_cols, paste0(include_cols, '_id'))) {
         tx <- sub('_id$', '', tx_col)
-        tx_tbl <- do.call(dplyr::select_, c(
-            list(dplyr::tbl(mdb$db, tx)),
-            named_list("name", tx, tx_col, tx_col),
-            NULL))
+        tx_tbl <- dplyr::select(
+            dplyr::tbl(mdb$db, tx),
+            {{tx_col}} := {{tx_col}},
+            {{tx}} := rlang::sym("name"))
         dp_tbl <- dplyr::left_join(dp_tbl, tx_tbl, by = tx_col)
     }
 
     # Hide all taxonomy id fields in the output
-    dp_tbl <- dplyr::select_(dp_tbl, ~ -one_of(tx_cols))
+    dp_tbl <- dplyr::select(dp_tbl, -dplyr::one_of(tx_cols))
 
     return(dp_tbl)
 }
