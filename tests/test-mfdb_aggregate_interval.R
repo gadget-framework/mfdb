a3519401934db4d03ea4d70c5641af2193e6054f
Jamie Lentin <jm@lentin.co.uk>
2020-12-11 09:55:47 +0000

    tests: Use native ut_cmp_identical

--- tests/test-mfdb_aggregate_interval.R
+++ tests/test-mfdb_aggregate_interval.R
@@ -6,44 +6,44 @@ g <- NULL
 mdb <- fake_mdb()
 
 ok_group("Can generate objects", {
-    expect_error(
+    ok(ut_cmp_error(
         mfdb_interval("l", c()),
-        "vect must at least be 2 items long")
-    expect_error(
+        "vect must at least be 2 items long"))
+    ok(ut_cmp_error(
         mfdb_interval("l", c(1)),
-        "vect must at least be 2 items long")
+        "vect must at least be 2 items long"))
 
-    expect_equal(
+    ok(ut_cmp_identical(
         class(mfdb_interval("l", c(1, 100))),
-        c("mfdb_interval", "mfdb_aggregate"))
+        c("mfdb_interval", "mfdb_aggregate")))
 })
 
 ok_group("Elements are named by prefixes", local({
-    expect_equal(
+    ok(ut_cmp_identical(
         agg_summary(mdb, mfdb_interval("l", c(10,20,25,30)), 'col', 'out', data.frame(), 0),
         list(
             l10 = structure(call("seq", 10, 19), min = 10, max = 20),
             l20 = structure(call("seq", 20, 24), min = 20, max = 25),
-            l25 = structure(call("seq", 25, 29), min = 25, max = 30)))
-    expect_equal(
+            l25 = structure(call("seq", 25, 29), min = 25, max = 30))))
+    ok(ut_cmp_identical(
         agg_summary(mdb, mfdb_interval("l", c(5,15,25,30)), 'col', 'out', data.frame(), 0),
         list(
             l5  = structure(call("seq",  5, 14), min =  5, max = 15),
             l15 = structure(call("seq", 15, 24), min = 15, max = 25),
-            l25 = structure(call("seq", 25, 29), min = 25, max = 30)))
+            l25 = structure(call("seq", 25, 29), min = 25, max = 30))))
 }, asNamespace('mfdb')))
 
 ok_group("Can generate SQL", local({
-    ok(cmp(
+    ok(ut_cmp_identical(
             select_clause(mdb, mfdb_interval("l", c(10,20,30)), "col", "outname"),
             "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 30::REAL THEN 'l20' ELSE 'l20' END AS outname"),
         "Generated select clause")
 
-    ok(cmp(
+    ok(ut_cmp_identical(
             where_clause(mdb, mfdb_interval("l", c(10,20,25,30)), "col", "out"),
             c("col >= 10::REAL", "col < 30::REAL")),
         "Generated where clause")
-    ok(cmp(
+    ok(ut_cmp_identical(
             where_clause(mdb, mfdb_interval("l", c(25,50)), "col", "out"),
             c("col >= 25::REAL", "col < 50::REAL")),
         "Generated where clause")
@@ -51,20 +51,20 @@ ok_group("Can generate SQL", local({
 
 ok_group("Aggregates with open_ended mfdb_interval", local({
     g <<- mfdb_interval('l', c(10, 20, 100), open_ended = TRUE)
-    ok(cmp(capture.output(pre_query(NULL, g, "out")), c(
+    ok(ut_cmp_identical(capture.output(pre_query(NULL, g, "out")), c(
         "NULL")), "Nothing happened pre_query")
-    ok(cmp(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
-    ok(cmp(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause")
-    ok(cmp(from_clause(mdb, g, "col", "out"), c()), "From clause")
-    ok(cmp(where_clause(mdb, g, "col", "out"), "col >= 10::REAL"), "Where clause")
+    ok(ut_cmp_identical(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause")
+    ok(ut_cmp_identical(from_clause(mdb, g, "col", "out"), c()), "From clause")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), "col >= 10::REAL"), "Where clause")
 
     g <<- mfdb_interval('l', c(10, 20, 100), open_ended = c('upper'))
-    ok(cmp(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause c('upper')")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause c('upper')")
 
     g <<- mfdb_interval('l', c(10, 20, 100), open_ended = c('lower'))
-    ok(cmp(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause c('lower')")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause c('lower')")
 
     g <<- mfdb_interval('l', c(10, 20, 100), open_ended = c('lower', 'upper'))
-    ok(cmp(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause c('lower', 'upper')")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "CASE WHEN col < 20::REAL THEN 'l10' WHEN col < 100::REAL THEN 'l20' ELSE 'l20' END AS out"), "Select clause c('lower', 'upper')")
 
 }, asNamespace('mfdb')))
--- tests/test-mfdb_aggregate_step_interval.R
+++ tests/test-mfdb_aggregate_step_interval.R
@@ -17,7 +17,7 @@ ok_group("Can generate objects", {
 
 ok_group("Can convert mfdb_step_intervals into lists", local({
     g <<- mfdb_step_interval('l', 10)
-    ok(cmp(
+    ok(ut_cmp_identical(
         agg_summary(mdb, mfdb_step_interval('l', 10, from = 10, to = 50), 'c.len', 'len', data.frame(), 0),
         list(
             l10 = structure(call("seq", 10, 19), min = 10, max = 20),
@@ -30,7 +30,7 @@ ok_group("Can convert mfdb_step_intervals into lists", local({
         agg_summary(mdb, mfdb_step_interval('l', 10), 'c.claire', 'claire', data.frame(), 0),
         "claire"), "Can't convert unbounded interval without data")
 
-    ok(cmp(
+    ok(ut_cmp_identical(
         agg_summary(mdb, mfdb_step_interval('l', 10), 'c.len', 'len', data.frame(len = c('l0', 'l20', 'l30', 'l20')), 0),
         list(
             l0  = structure(call("seq",  0,  9), min =  0, max = 10),
@@ -38,7 +38,7 @@ ok_group("Can convert mfdb_step_intervals into lists", local({
             l20 = structure(call("seq", 20, 29), min = 20, max = 30),
             l30 = structure(call("seq", 30, 39), min = 30, max = 40))),
         "Can convert unbounded interval using returned data (max: 30)")
-    ok(cmp(
+    ok(ut_cmp_identical(
         agg_summary(mdb, mfdb_step_interval('l', 10), 'c.len', 'len', data.frame(len = c('l0', 'l20', 'l30', 'l40')), 0),
         list(
             l0  = structure(call("seq",  0,  9), min =  0, max = 10),
@@ -48,7 +48,7 @@ ok_group("Can convert mfdb_step_intervals into lists", local({
             l40 = structure(call("seq", 40, 49), min = 40, max = 50))),
         "Can convert unbounded interval using returned data (max: 40)")
 
-    ok(cmp(
+    ok(ut_cmp_identical(
         agg_summary(mdb, mfdb_step_interval('l', 10, from = 10, to = 50, open_ended = c('upper')), 'c.len', 'len', data.frame(), 0),
         list(
             l10 = structure(call("seq", 10, 19), min = 10, max = 20),
@@ -60,45 +60,45 @@ ok_group("Can convert mfdb_step_intervals into lists", local({
 
 ok_group("Aggregates with close_ended mfdb_step_interval", local({
     g <<- mfdb_step_interval('l', 10)
-    ok(cmp(capture.output(pre_query(NULL, g, "col")), c(
+    ok(ut_cmp_identical(capture.output(pre_query(NULL, g, "col")), c(
         "NULL")), "Nothing happened pre_query")
-    ok(cmp(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
-    ok(cmp(select_clause(mdb, g, "col", "out"), "'l' || (greatest(floor(col)::integer, 0) / 10) * 10 AS out"), "Select clause")
-    ok(cmp(from_clause(mdb, g, "col", "out"), c()), "From clause")
-    ok(cmp(where_clause(mdb, g, "col", "out"), "col >= 0"), "Where clause")
+    ok(ut_cmp_identical(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "'l' || (greatest(floor(col)::integer, 0) / 10) * 10 AS out"), "Select clause")
+    ok(ut_cmp_identical(from_clause(mdb, g, "col", "out"), c()), "From clause")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), "col >= 0"), "Where clause")
 
     g <<- mfdb_step_interval('l', 5, from = 10, to = 90)
-    ok(cmp(capture.output(pre_query(NULL, g, "col")), c(
+    ok(ut_cmp_identical(capture.output(pre_query(NULL, g, "col")), c(
         "NULL")), "Nothing happened pre_query")
-    ok(cmp(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
-    ok(cmp(select_clause(mdb, g, "col", "out"), "'l' || (least(greatest(floor(col)::integer, 10), 85) / 5) * 5 AS out"), "Select clause")
-    ok(cmp(from_clause(mdb, g, "col", "out"), c()), "From clause")
-    ok(cmp(where_clause(mdb, g, "col", "out"), c("col >= 10", "col < 90")), "Where clause")
+    ok(ut_cmp_identical(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "'l' || (least(greatest(floor(col)::integer, 10), 85) / 5) * 5 AS out"), "Select clause")
+    ok(ut_cmp_identical(from_clause(mdb, g, "col", "out"), c()), "From clause")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), c("col >= 10", "col < 90")), "Where clause")
 }, asNamespace('mfdb')))
 
 ok_group("Aggregates with open_ended mfdb_step_interval", local({
     g <<- mfdb_step_interval('l', 10, open_ended = TRUE)
-    ok(cmp(capture.output(pre_query(NULL, g, "col")), c(
+    ok(ut_cmp_identical(capture.output(pre_query(NULL, g, "col")), c(
         "NULL")), "Nothing happened pre_query")
-    ok(cmp(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
-    ok(cmp(select_clause(mdb, g, "col", "out"), "'l' || (greatest(floor(col)::integer, 0) / 10) * 10 AS out"), "Select clause")
-    ok(cmp(from_clause(mdb, g, "col", "out"), c()), "From clause")
-    ok(cmp(where_clause(mdb, g, "col", "out"), "col >= 0"), "Where clause")
+    ok(ut_cmp_identical(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "'l' || (greatest(floor(col)::integer, 0) / 10) * 10 AS out"), "Select clause")
+    ok(ut_cmp_identical(from_clause(mdb, g, "col", "out"), c()), "From clause")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), "col >= 0"), "Where clause")
 
     g <<- mfdb_step_interval('l', 5, from = 10, to = 90, open_ended = TRUE)
-    ok(cmp(capture.output(pre_query(NULL, g, "col")), c(
+    ok(ut_cmp_identical(capture.output(pre_query(NULL, g, "col")), c(
         "NULL")), "Nothing happened pre_query")
-    ok(cmp(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
-    ok(cmp(select_clause(mdb, g, "col", "out"), "'l' || (least(greatest(floor(col)::integer, 10), 85) / 5) * 5 AS out"), "Select clause")
-    ok(cmp(from_clause(mdb, g, "col", "out"), c()), "From clause")
-    ok(cmp(where_clause(mdb, g, "col", "out"), c("col >= 10")), "Where clause")
+    ok(ut_cmp_identical(sample_clause(mdb, g, "col", "out"), "0"), "Sample clause")
+    ok(ut_cmp_identical(select_clause(mdb, g, "col", "out"), "'l' || (least(greatest(floor(col)::integer, 10), 85) / 5) * 5 AS out"), "Select clause")
+    ok(ut_cmp_identical(from_clause(mdb, g, "col", "out"), c()), "From clause")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), c("col >= 10")), "Where clause")
 
     g <<- mfdb_step_interval('l', 5, from = 10, to = 90, open_ended = c('upper'))
-    ok(cmp(where_clause(mdb, g, "col", "out"), c("col >= 10")), "Where clause c('upper')")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), c("col >= 10")), "Where clause c('upper')")
 
     g <<- mfdb_step_interval('l', 5, from = 10, to = 90, open_ended = c('lower'))
-    ok(cmp(where_clause(mdb, g, "col", "out"), c("col < 90")), "Where clause c('lower')")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), c("col < 90")), "Where clause c('lower')")
 
     g <<- mfdb_step_interval('l', 5, from = 10, to = 90, open_ended = c('upper', 'lower'))
-    ok(cmp(where_clause(mdb, g, "col", "out"), c()), "Where clause c('upper', 'lower')")
+    ok(ut_cmp_identical(where_clause(mdb, g, "col", "out"), c()), "Where clause c('upper', 'lower')")
 }, asNamespace('mfdb')))
