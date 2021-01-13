11bce444e5d60689911cdbc347af0c884eed5b8e
Jamie Lentin <jm@lentin.co.uk>
2021-01-13 12:20:28 +0000

    inttest-helpers: Quieten Sys.timezone()
    Sys.timezone() will produce spurious warnings on first call depending
    on environment. Call it now so we don't pollute actual tests runs with
    it.

--- tests/utils/inttest-helpers.R
+++ tests/utils/inttest-helpers.R
@@ -6,3 +6,13 @@ db_params <- list(dbname = "mf_inttest")
 db_connection <- function() {
     do.call(dbConnect, c(list(drv = PostgreSQL()), db_params))
 }
+
+# NB: Sys.timezone() will produce spurious warnings on first call depending on environment:
+# > Sys.timezone()
+# Failed to create bus connection: No such file or directory
+# [1] "Europe/London"
+# Warning message:
+# In system("timedatectl", intern = TRUE) :
+#   running command 'timedatectl' had status 1
+# https://bugs.r-project.org/bugzilla/show_bug.cgi?id=17421
+suppressWarnings(Sys.timezone())
