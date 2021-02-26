706b61fcef435594df2c4e9eb4bf58f8a4316448
Jamie Lentin <jm@lentin.co.uk>
2021-02-26 17:52:13 +0000

    mfdb: Schema-juggling pointless for SQLite
    Half of this is backwards compatibility anyway, and very PG specific.

--- R/mfdb.R
+++ R/mfdb.R
@@ -81,103 +81,113 @@ mfdb <- function(case_study_name = "",
             schema = if (nzchar(case_study_name)) gsub('\\W', '_', tolower(case_study_name)) else "public",
             db_args = db_combined,
             db = db_connection), class = "mfdb")
-    schema_count <- mfdb_fetch(mdb,
-        "SELECT COUNT(*)",
-        " FROM pg_catalog.pg_namespace",
-        " WHERE nspname IN ", sql_quote(mdb$schema, always_bracket = TRUE))[, c(1)]
+    if (mfdb_is_postgres(mdb)) {
+        schema_count <- mfdb_fetch(mdb,
+            "SELECT COUNT(*)",
+            " FROM pg_catalog.pg_namespace",
+            " WHERE nspname IN ", sql_quote(mdb$schema, always_bracket = TRUE))[, c(1)]
 
-    if (!nzchar(case_study_name)) {
-        names <- mfdb_fetch(mdb,
-            "SELECT table_schema",
-            " FROM information_schema.tables",
-            " WHERE table_schema != 'public' AND table_name = 'mfdb_schema'")
-        names <- if (ncol(names) > 0) names[,1] else c()
-        stop("You must supply a schema name for case_study_name to use in the database. Available names:-\n", paste0("* ", names, collapse = "\n"))
-    }
-
-    if (destroy_schema) {
-        if (schema_count == 0) {
-            mdb$logger$info(paste0("Schema ", mdb$schema, " does not exist. Doing nothing"))
-        } else {
-            mfdb_destroy_schema(mdb)
-            mdb$logger$info(paste0("Schema ", mdb$schema, " removed, connect again to repopulate DB."))
+        if (!nzchar(case_study_name)) {
+            names <- mfdb_fetch(mdb,
+                "SELECT table_schema",
+                " FROM information_schema.tables",
+                " WHERE table_schema != 'public' AND table_name = 'mfdb_schema'")
+            names <- if (ncol(names) > 0) names[,1] else c()
+            stop("You must supply a schema name for case_study_name to use in the database. Available names:-\n", paste0("* ", names, collapse = "\n"))
         }
-        dbDisconnect(mdb$db)
-        return(invisible(NULL))
-    }
 
-    if (case_study_name == 'public') {
-        stop("Can't connect to the public schema, since the database tables will be different. ",
-            "Instead, use the case study name and copy the data out of public")
-    }
-
-    # Update schema and taxonomies
-    if (schema_count > 0) {
-        mfdb_send(mdb, "SET search_path TO ", paste(mdb$schema, 'pg_temp', sep =","))
-    } else {
-        logger$info(paste0("No schema, creating ", mdb$schema))
-        mfdb_transaction(mdb, {
-            mfdb_send(mdb, "CREATE SCHEMA ", mdb$schema)
-        })
-        mfdb_send(mdb, "SET search_path TO ", paste(mdb$schema, 'pg_temp', sep =","))
-
-        # If schema didn't exist before, see if there's data to be had in the old public tables
-        if (mfdb_table_exists(mdb, 'case_study', schema_name = 'public')) {
-            res <- mfdb_fetch(mdb,
-                "SELECT case_study_id",
-                " FROM public.case_study",
-                " WHERE name = ", sql_quote(case_study_name))
-        } else {
-            res <- c()
-        }
-        if (length(res) > 0 && nrow(res) > 0) {
-            logger$info(paste0("Copying data from ", case_study_name))
-            # A case study exists by this ID
-            old_case_study_id <- res[1,1]
-
-            # Upgrade the old database to a known state
-            mdb_public <- structure(list(
-                logger = mdb$logger,
-                save_temp_tables = mdb$save_temp_tables,
-                state = mdb$state,
-                schema = "public",
-                db = mdb$db), class = "mfdb")
-            mfdb_update_schema(mdb_public, target_version = 4)
-
-            # Create new schema, to known state
-            mfdb_update_schema(mdb, target_version = 7)
-
-            # Copy data from old tables
-            mfdb4_cs_taxonomy <- c("areacell", "sampling_type", "data_source", "index_type", "tow", "vessel")
-            mfdb4_measurement_tables <- c('survey_index', 'division', 'sample', 'predator', 'prey')
-            for (table_name in c(mfdb4_cs_taxonomy, mfdb4_measurement_tables)) {
-                cols <- mfdb_fetch(mdb, "SELECT column_name",
-                    " FROM information_schema.columns",
-                    " WHERE table_schema = 'public'",
-                    " AND table_name = ", sql_quote(table_name),
-                    NULL
-                )[,1]
-                cols <- cols[cols != "case_study_id"]
-
-                mfdb_send(mdb,
-                    "INSERT INTO ", table_name,
-                    " (", paste(cols, collapse = ","), ")",
-                    " SELECT ", paste(cols, collapse = ","),
-                    " FROM public.", table_name,
-                    if (table_name == 'prey')
-                        c(" WHERE predator_id IN (SELECT DISTINCT predator_id FROM public.predator WHERE case_study_id = ", old_case_study_id, ")")
-                    else if (table_name == 'index_type')
-                        c(" WHERE case_study_id = ", old_case_study_id, " AND index_type_id != 99999")
-                    else
-                        c(" WHERE case_study_id = ", old_case_study_id),
-                    "");
-
-                # Ensure the sequence is up to date for the table
-                table_primary_key <- paste0(table_name, '_id')
-                mfdb_fetch(mdb,
-                    "SELECT pg_catalog.setval(pg_get_serial_sequence('", table_name, "', '", table_primary_key, "'), MAX(", table_primary_key, "))",
-                    " FROM ", table_name)
+        if (destroy_schema) {
+            if (schema_count == 0) {
+                mdb$logger$info(paste0("Schema ", mdb$schema, " does not exist. Doing nothing"))
+            } else {
+                mfdb_destroy_schema(mdb)
+                mdb$logger$info(paste0("Schema ", mdb$schema, " removed, connect again to repopulate DB."))
             }
+            dbDisconnect(mdb$db)
+            return(invisible(NULL))
+        }
+
+        if (case_study_name == 'public') {
+            stop("Can't connect to the public schema, since the database tables will be different. ",
+                "Instead, use the case study name and copy the data out of public")
+        }
+
+        # Update schema and taxonomies
+        if (schema_count > 0) {
+            mfdb_send(mdb, "SET search_path TO ", paste(mdb$schema, 'pg_temp', sep =","))
+        } else {
+            logger$info(paste0("No schema, creating ", mdb$schema))
+            mfdb_transaction(mdb, {
+                mfdb_send(mdb, "CREATE SCHEMA ", mdb$schema)
+            })
+            mfdb_send(mdb, "SET search_path TO ", paste(mdb$schema, 'pg_temp', sep =","))
+
+            # If schema didn't exist before, see if there's data to be had in the old public tables
+            if (mfdb_table_exists(mdb, 'case_study', schema_name = 'public')) {
+                res <- mfdb_fetch(mdb,
+                    "SELECT case_study_id",
+                    " FROM public.case_study",
+                    " WHERE name = ", sql_quote(case_study_name))
+            } else {
+                res <- c()
+            }
+            if (length(res) > 0 && nrow(res) > 0) {
+                logger$info(paste0("Copying data from ", case_study_name))
+                # A case study exists by this ID
+                old_case_study_id <- res[1,1]
+
+                # Upgrade the old database to a known state
+                mdb_public <- structure(list(
+                    logger = mdb$logger,
+                    save_temp_tables = mdb$save_temp_tables,
+                    state = mdb$state,
+                    schema = "public",
+                    db = mdb$db), class = "mfdb")
+                mfdb_update_schema(mdb_public, target_version = 4)
+
+                # Create new schema, to known state
+                mfdb_update_schema(mdb, target_version = 7)
+
+                # Copy data from old tables
+                mfdb4_cs_taxonomy <- c("areacell", "sampling_type", "data_source", "index_type", "tow", "vessel")
+                mfdb4_measurement_tables <- c('survey_index', 'division', 'sample', 'predator', 'prey')
+                for (table_name in c(mfdb4_cs_taxonomy, mfdb4_measurement_tables)) {
+                    cols <- mfdb_fetch(mdb, "SELECT column_name",
+                        " FROM information_schema.columns",
+                        " WHERE table_schema = 'public'",
+                        " AND table_name = ", sql_quote(table_name),
+                        NULL
+                    )[,1]
+                    cols <- cols[cols != "case_study_id"]
+
+                    mfdb_send(mdb,
+                        "INSERT INTO ", table_name,
+                        " (", paste(cols, collapse = ","), ")",
+                        " SELECT ", paste(cols, collapse = ","),
+                        " FROM public.", table_name,
+                        if (table_name == 'prey')
+                            c(" WHERE predator_id IN (SELECT DISTINCT predator_id FROM public.predator WHERE case_study_id = ", old_case_study_id, ")")
+                        else if (table_name == 'index_type')
+                            c(" WHERE case_study_id = ", old_case_study_id, " AND index_type_id != 99999")
+                        else
+                            c(" WHERE case_study_id = ", old_case_study_id),
+                        "");
+
+                    # Ensure the sequence is up to date for the table
+                    table_primary_key <- paste0(table_name, '_id')
+                    mfdb_fetch(mdb,
+                        "SELECT pg_catalog.setval(pg_get_serial_sequence('", table_name, "', '", table_primary_key, "'), MAX(", table_primary_key, "))",
+                        " FROM ", table_name)
+                }
+            }
+        }
+    } else if (mfdb_is_sqlite(mdb)) {
+        # A sqlite database doesn't have separate schema
+        if (destroy_schema) {
+            stop('TODO: Delete all tables')
+            mdb$logger$info(paste0("Schema ", mdb$schema, " removed, connect again to repopulate DB."))
+            dbDisconnect(mdb$db)
+            return(invisible(NULL))
         }
     }
 
