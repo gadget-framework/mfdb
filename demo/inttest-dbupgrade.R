4112a0224a2b3fda496a2298924cf67ea28919b2
Jamie Lentin <jm@lentin.co.uk>
2021-04-23 16:50:38 +0100

    demo/inttest-dbupgrade: Only run tests on Postgresql
    These are only going to work for postgres, since they import PG dumps.

--- demo/inttest-dbupgrade.R
+++ demo/inttest-dbupgrade.R
@@ -19,64 +19,68 @@ mdb <- mfdb('inttest-dbupgrade', db_params = db_params)
 db_args <- mdb$db_args
 mfdb_disconnect(mdb)
 
-# Rebuild the version 2 schema
-mdb <- mfdb('Test', db_params = db_params, destroy_schema = TRUE)
-system(paste(
-    'echo "DROP SCHEMA public CASCADE" | psql',
-    ' --host=', db_args$host,
-    ' --dbname=', db_args$dbname,
-    sep="", collapse=""))
-system(paste(
-    'echo "DROP SCHEMA test CASCADE" | psql',
-    ' --host=', db_args$host,
-    ' --dbname=', db_args$dbname,
-    sep="", collapse=""))
-system(paste(
-    'psql',
-    ' --host=', db_args$host,
-    ' --dbname=', db_args$dbname,
-    ' --file=tests/utils/schema_2.x.sql',
-    sep="", collapse=""))
+if (db_args$dbname != ':memory:') {
+    # Rebuild the version 2 schema
+    mdb <- mfdb('Test', db_params = db_params, destroy_schema = TRUE)
+    system(paste(
+        'echo "DROP SCHEMA public CASCADE" | psql',
+        ' --host=', db_args$host,
+        ' --dbname=', db_args$dbname,
+        sep="", collapse=""))
+    system(paste(
+        'echo "DROP SCHEMA test CASCADE" | psql',
+        ' --host=', db_args$host,
+        ' --dbname=', db_args$dbname,
+        sep="", collapse=""))
+    system(paste(
+        'psql',
+        ' --host=', db_args$host,
+        ' --dbname=', db_args$dbname,
+        ' --file=tests/utils/schema_2.x.sql',
+        sep="", collapse=""))
 
-# Create a proper MFDB object, should upgrade to latest version
-mdb <- mfdb('Test', db_params = db_params, save_temp_tables = FALSE)
-ok(cmp(
-    mfdb:::mfdb_fetch(mdb, "SELECT MAX(version) FROM mfdb_schema")[1,1],
-    as.integer(gsub("\\..*", "", packageVersion("mfdb")))), "Database now at latest release")
+    # Create a proper MFDB object, should upgrade to latest version
+    mdb <- mfdb('Test', db_params = db_params, save_temp_tables = FALSE)
+    ok(cmp(
+        mfdb:::mfdb_fetch(mdb, "SELECT MAX(version) FROM mfdb_schema")[1,1],
+        as.integer(gsub("\\..*", "", packageVersion("mfdb")))), "Database now at latest release")
 
-# Can add more data after upgrading
-mfdb_import_survey(mdb, data_source = "had_post_upgrade", table_string("
-year    month   areacell        species length  age     weight
-2000    1       45G01           HAD     21      2       210
-    "))
+    # Can add more data after upgrading
+    mfdb_import_survey(mdb, data_source = "had_post_upgrade", table_string("
+    year    month   areacell        species length  age     weight
+    2000    1       45G01           HAD     21      2       210
+        "))
 
-# Check schema name edge cases
-ok(cmp_error(
-    mfdb('public', db_params = db_params, save_temp_tables = FALSE),
-    "public"), "Can't connect to the public schema")
-ok(cmp_error(
-    mfdb('', db_params = db_params, save_temp_tables = FALSE, destroy_schema = TRUE),
-    "case_study_name[^X]*\\* test"), "Not allowed to give an empty case_study name when destroying, given list of schemas")
-ok(cmp_error(
-    mfdb('', db_params = db_params, save_temp_tables = FALSE),
-    "case_study_name[^X]*\\* test"), "Not allowed to give an empty case_study name, test schema still there")
+    # Check schema name edge cases
+    ok(cmp_error(
+        mfdb('public', db_params = db_params, save_temp_tables = FALSE),
+        "public"), "Can't connect to the public schema")
+    ok(cmp_error(
+        mfdb('', db_params = db_params, save_temp_tables = FALSE, destroy_schema = TRUE),
+        "case_study_name[^X]*\\* test"), "Not allowed to give an empty case_study name when destroying, given list of schemas")
+    ok(cmp_error(
+        mfdb('', db_params = db_params, save_temp_tables = FALSE),
+        "case_study_name[^X]*\\* test"), "Not allowed to give an empty case_study name, test schema still there")
 
-# Drop public schema so we don't import this data later
-mdb <- mfdb('public', db_params = db_params, destroy_schema = TRUE)
+    # Drop public schema so we don't import this data later
+    mdb <- mfdb('public', db_params = db_params, destroy_schema = TRUE)
+}
 
-# Rebuild a version 5 schema and upgrade it
-system(paste(
-    'echo "DROP SCHEMA test_5x CASCADE" | psql',
-    ' --host=', db_args$host,
-    ' --dbname=', db_args$dbname,
-    sep="", collapse=""))
-system(paste(
-    'pg_restore',
-    ' --host=', db_args$host,
-    ' --dbname=', db_args$dbname,
-    ' tests/utils/schema_5.x.dump',
-    sep="", collapse=""))
-mdb <- mfdb('test_5x', db_params = db_params)
-ok(cmp(
-    mfdb:::mfdb_fetch(mdb, "SELECT MAX(version) FROM mfdb_schema")[1,1],
-    as.integer(gsub("\\..*", "", packageVersion("mfdb")))), "Database now at latest release")
+if (db_args$dbname != ':memory:') {
+    # Rebuild a version 5 schema and upgrade it
+    system(paste(
+        'echo "DROP SCHEMA test_5x CASCADE" | psql',
+        ' --host=', db_args$host,
+        ' --dbname=', db_args$dbname,
+        sep="", collapse=""))
+    system(paste(
+        'pg_restore',
+        ' --host=', db_args$host,
+        ' --dbname=', db_args$dbname,
+        ' tests/utils/schema_5.x.dump',
+        sep="", collapse=""))
+    mdb <- mfdb('test_5x', db_params = db_params)
+    ok(cmp(
+        mfdb:::mfdb_fetch(mdb, "SELECT MAX(version) FROM mfdb_schema")[1,1],
+        as.integer(gsub("\\..*", "", packageVersion("mfdb")))), "Database now at latest release")
+}
