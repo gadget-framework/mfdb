c5505b40f9a2e606bae6a1c211fa25d0fe2867e8
Jamie Lentin <jm@lentin.co.uk>
2021-06-04 13:07:54 +0100

    mfdb_dplyr: Rework to recursively include metadata #75
    The previous join logic couldn't recurse into further tables, e.g.
    trip_start_port_latitude.
    
    Switch to using dbplyr 2.0 in the process.

--- R/mfdb_dplyr.R
+++ R/mfdb_dplyr.R
@@ -2,33 +2,84 @@
 all_cols <- c("$ALL")
 
 # Fetch a dplyr version of any measurement table
-mfdb_dplyr_table <- function (mdb, tbl_name, include_cols) {
-    # Turn mdb connection into dplyr & get table
-    #dp <- mdb$db#dbplyr::src_sql("postgres", mdb$db, info = DBI::dbGetInfo(mdb$db))
-    dp_tbl <- dplyr::tbl(mdb$db, tbl_name)
+mfdb_dplyr_table <- function (mdb, table_name, include_cols, prefix = c()) {
+    # Join prefixes together without extra _
+    prefix_join <- function (...) paste(c(...), collapse = "_")
 
-    # Get all possible taxonomy columns in this table
-    tx_cols <- intersect(
-        paste0(mfdb_taxonomy_tables, '_id'),
-        colnames(dp_tbl))
-
-    # Replace all_cols marker with real thing
+    # Resolve all_cols using col_defs_for
     if (identical(include_cols, all_cols)) {
-        include_cols <- mfdb_taxonomy_tables
+        include_cols <- names(col_defs_for(table_name))
+    }
+    # Empty string (i.e. the table name from outer step) is actually a reference to the name col
+    include_cols <- gsub("^$", "name", include_cols)
+
+    # Find column definitions, turn into matrix for easy access
+    if (table_name %in% names(mfdb_measurement_table_defs)) {
+        col_defs <- mfdb_measurement_table_defs[[table_name]]$cols
+    } else if (table_name %in% names(mfdb_taxonomy_table_defs)) {
+        # TODO: We should sort out these definitions
+        col_defs <- c(
+            "name", "VARCHAR(1024) UNIQUE NOT NULL", "Short name used in data files / output data (in ltree notation)",
+            "t_group", paste0("VARCHAR(1024) NULL REFERENCES ", table_name, "(name)"), "Value grouping (short name)",
+            mfdb_taxonomy_table_defs[[table_name]]$cols)
+    } else {
+        stop("Unknown table ", table_name)
+    }
+    col_defs <- t(matrix(col_defs, nrow = 3))
+
+    select_cols <- c(paste0(table_name, '_id'))  # NB: Need ID for join to work
+    join_tables <- as.environment(list())
+    join_by <- as.environment(list())
+    for (i in seq_len(nrow(col_defs))) {
+        if (col_defs[i,1] != 't_group' && grepl('REFERENCES', col_defs[i,2], fixed = TRUE)) {
+            fk <- col_def_get_foreign_key(col_defs[i,2])
+            # NB: sub_prefix != fk$table, e.g. start_port
+            sub_prefix <- gsub("_id$", "", col_defs[i,1])
+
+            # Find all prefixed entries in include cols, remove prefix
+            sub_include_cols <- grep(paste0('^', sub_prefix, '($|_)'), include_cols, value = TRUE)
+            sub_include_cols <- gsub(paste0('^', sub_prefix, '($|_)'), '', sub_include_cols)
+
+            if (length(sub_include_cols) > 0) {
+                # Make sure we have the foreign key to join with
+                select_cols <- c(select_cols, col_defs[i, 1])
+                # Get the sub-table so we can join it later
+                join_tables[[prefix_join(prefix, sub_prefix)]] <- mfdb_dplyr_table(
+                    mdb,
+                    fk$table,
+                    sub_include_cols,
+                    prefix = prefix_join(prefix, sub_prefix))
+                join_by[[prefix_join(prefix, sub_prefix)]] <- structure(
+                    # i.e. final name of ID column in subtable
+                    prefix_join(prefix, sub_prefix, fk$column),
+                    # i.e. final name of ID column here
+                    names = prefix_join(prefix, col_defs[i, 1]))
+            }
+        } else if (length(prefix) == 0) {
+            # In the outerermost table we include all measurements
+            select_cols <- c(select_cols, col_defs[i, 1])
+        } else {
+            select_cols <- c(select_cols, intersect(col_defs[i, 1], include_cols))
+        }
     }
 
-    # Left-join each desired taxonomy table
-    for (tx_col in intersect(tx_cols, paste0(include_cols, '_id'))) {
-        tx <- sub('_id$', '', tx_col)
-        tx_tbl <- dplyr::select(
-            dplyr::tbl(mdb$db, tx),
-            {{tx_col}} := {{tx_col}},
-            {{tx}} := rlang::sym("name"))
-        dp_tbl <- dplyr::left_join(dp_tbl, tx_tbl, by = tx_col)
+    # Select all requested columns
+    dp_tbl <- dplyr::tbl(mdb$db, table_name)
+    dp_tbl <- dplyr::select(dp_tbl, dplyr::any_of(select_cols))
+    dp_tbl <- dplyr::rename_with(dp_tbl, function (names) vapply(names, function (n) {
+        # Add prefix to all columns, Rename name -> (prefix)
+        ifelse(n == 'name', prefix, prefix_join(prefix, n))
+    }, character(1)))
+
+    # Do the joins now the initial selection is done
+    for (j in ls(join_tables)) {
+        dp_tbl <- dplyr::left_join(dp_tbl, join_tables[[j]], by = join_by[[j]])
     }
 
-    # Hide all taxonomy id fields in the output
-    dp_tbl <- dplyr::select(dp_tbl, -dplyr::one_of(tx_cols))
+    if (length(prefix) == 0) {
+        # Outermost table, remove all _id cols now
+        dp_tbl <- dplyr::select(dp_tbl, -dplyr::ends_with('_id'))
+    }
 
     return(dp_tbl)
 }
--- demo/inttest-dplyr.R
+++ demo/inttest-dplyr.R
@@ -49,7 +49,6 @@ year    month   areacell        species sampling_type length  age     weight
 
     # Connect to the table, don't fetch any extra data
     ok(cmp(to_df(mfdb_dplyr_sample(mdb, c())), data.frame(
-        sample_id = seq(12),
         year = as.integer(2000),
         month = as.integer(1),
         age = as.numeric(c(2, 3, 3, 1, 1, 2, 2, 3, 3, 1, 1, 2)),
@@ -69,8 +68,7 @@ year    month   areacell        species sampling_type length  age     weight
         count = c(1),
         stringsAsFactors = FALSE)), "No taxonomy tables")
 
-    ok(cmp(to_df(mfdb_dplyr_sample(mdb, c('sampling_type', 'species'))), data.frame(
-        sample_id = seq(12),
+    ok(cmp(to_df(mfdb_dplyr_sample(mdb, c('sampling_type', 'length', 'species'))), data.frame(
         year = as.integer(2000),
         month = as.integer(1),
         age = as.numeric(c(2, 3, 3, 1, 1, 2, 2, 3, 3, 1, 1, 2)),
@@ -88,40 +86,39 @@ year    month   areacell        species sampling_type length  age     weight
         gutted_weight = as.numeric(NA),
         gutted_weight_var = as.numeric(NA),
         count = c(1),
-        species = rep(c('COD', 'HAD'), each = 3, times = 2),
         sampling_type = rep(c('comm', 'res'), each = 6),
+        species = rep(c('COD', 'HAD'), each = 3, times = 2),
         stringsAsFactors = FALSE)), "Select taxonomy tables")
 
-    ok(cmp(to_df(mfdb_dplyr_sample(mdb)), data.frame(
-        sample_id = seq(12),
-        year = as.integer(2000),
-        month = as.integer(1),
-        age = as.integer(c(2, 3, 3, 1, 1, 2, 2, 3, 3, 1, 1, 2)),
-        length = c(21, 34, 34, 62, 53, 54, 28, 34, 24, 12, 44, 14),
-        length_var = as.numeric(NA),
-        length_min = as.integer(NA),
-        weight = c(210, 220, 230, 320, 330, 430, 210, 220, 230, 320, 330, 430),
-        weight_var = as.numeric(NA),
-        liver_weight = as.numeric(NA),
-        liver_weight_var = as.numeric(NA),
-        gonad_weight = as.numeric(NA),
-        gonad_weight_var = as.numeric(NA),
-        stomach_weight = as.numeric(NA),
-        stomach_weight_var = as.numeric(NA),
-        gutted_weight = as.numeric(NA),
-        gutted_weight_var = as.numeric(NA),
-        count = c(1),
-        institute = as.character(NA),
-        gear = as.character(NA),
-        sex = as.character(NA),
-        maturity_stage = as.character(NA),
-        species = rep(c('COD', 'HAD'), each = 3, times = 2),
-        areacell = rep(c("45G01", "45G02", "45G03"), times = 4),
-        sampling_type = rep(c('comm', 'res'), each = 6),
-        population = as.character(NA),
-        data_source = c("ds2000"),
-        trip = as.character(NA),
-        tow = as.character(NA),
-        vessel = as.character(NA),
-        stringsAsFactors = FALSE)), "All taxonomy tables")
+    df <- to_df(mfdb_dplyr_sample(mdb))
+    if (mfdb:::mfdb_is_sqlite(mdb)) df$age <- as.numeric(df$age)
+    ok(ut_cmp_identical(
+       df[c('year', 'month', 'age', 'weight', 'areacell', 'areacell_size', 'sampling_type', 'species')],
+       data.frame(
+           year = as.integer(2000),
+           month = as.integer(1),
+           age = as.numeric(c(2, 3, 3, 1, 1, 2, 2, 3, 3, 1, 1, 2)),
+           weight = c(210, 220, 230, 320, 330, 430, 210, 220, 230, 320, 330, 430),
+           areacell = rep(c("45G01", "45G02", "45G03"), times = 4),
+           areacell_size = rep(c(10, 200, 400), times = 4),
+           sampling_type = rep(c('comm', 'res'), each = 6),
+           species = rep(c('COD', 'HAD'), each = 3, times = 2),
+           stringsAsFactors = FALSE)), "All available, non-NA data matches")
+    ok(setequal(names(df), c(
+        "year", "month", "age", "length", "length_var", "length_min",
+        "weight", "weight_var", "liver_weight", "liver_weight_var", "gonad_weight",
+        "gonad_weight_var", "stomach_weight", "stomach_weight_var", "gutted_weight",
+        "gutted_weight_var", "count", "areacell", "areacell_size", "areacell_depth",
+        "data_source", "gear", "gear_mesh_size", "gear_mesh_size_min",
+        "gear_mesh_size_max", "institute", "maturity_stage", "population",
+        "sampling_type", "sex", "species", "tow", "tow_latitude", "tow_longitude",
+        "tow_end_latitude", "tow_end_longitude", "tow_start", "tow_depth",
+        "tow_length", "tow_duration", "tow_hook_count", "tow_net_count",
+        "tow_mesh_size", "tow_bait_type", "tow_net_type", "trip", "trip_start_date",
+        "trip_end_date", "trip_crew", "trip_oil_consumption", "trip_end_port",
+        "trip_end_port_latitude", "trip_end_port_longitude", "trip_end_port_institute",
+        "trip_start_port", "trip_start_port_latitude", "trip_start_port_longitude",
+        "trip_start_port_institute", "vessel", "vessel_full_name", "vessel_length",
+        "vessel_power", "vessel_tonnage", "vessel_vessel_owner_full_name",
+        NULL)), "All available, rest of column names match")
 })
